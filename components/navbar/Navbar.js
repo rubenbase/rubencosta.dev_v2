35a1b437a57309db034c1076029fc3869919984a
Ruben Costa <daggosso2@gmail.com>
2020-12-02 23:57:05 +0000

    create navbar

---
+++ components/navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import Headroom from 'react-headroom'
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavContainer,
+  NavMenu,
+  NavBtnLink,
+  NavBtn,
+} from './navbar-elements'
+
+// import { Container } from './styles';
+
+function Navbar() {
+  return (
+    <Headroom>
+      <Nav>
+        <NavContainer>
+          <NavLink href="/">
+            <h1>Hosply</h1>
+          </NavLink>
+          <Bars />
+          <NavMenu>
+            <NavLink href="/why">Why Hosply</NavLink>
+            <NavLink href="/solutions">Solutions</NavLink>
+            <NavLink href="/products">Products</NavLink>
+            <NavLink href="/pricing">Pricing</NavLink>
+            <NavLink href="/contact">Contact us</NavLink>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink href="/app">Get started for free</NavBtnLink>
+          </NavBtn>
+        </NavContainer>
+      </Nav>
+    </Headroom>
+  )
+}
+
+export default Navbar
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Navbar from 'components/navbar'
+
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main style={{ marginBottom: '230em' }} className={styles.main}>
+        <h3>Main content</h3>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </>
+  )
+}
