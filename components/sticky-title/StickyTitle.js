55f2b0b5b44b8d09b8526402f04897108dcd4976
Ruben Costa <daggosso2@gmail.com>
2021-01-31 23:52:20 +0000

    update sticky-title

--- components/sticky-title/StickyTitle.js
+++ components/sticky-title/StickyTitle.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 import styles from './styles.module.css'
+import clsx from 'clsx'
 
-function StickyTitle({ number, title }) {
+function StickyTitle({ number, title, bg }) {
   return (
-    <div className="border-l border-r w-sticky-title h-full fixed flex-shrink-0 z-10">
+    <div
+      className={clsx(
+        'border-l border-r w-sticky-title h-full fixed flex-shrink-0 z-10',
+        styles[bg]
+      )}
+    >
       <ul className={styles.rotateText}>
         <li className={styles.list}>
           <div className="flex items-center">{title}</div>
